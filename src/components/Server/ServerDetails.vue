03d62bbfe20ab74a8b65299a69ab67e4ea4ccd53
Franck Koukal <47780261+franckkoukal@users.noreply.github.com>
2019-04-22 17:13:09 +0200

    Add files via upload

---
+++ src/components/Server/ServerDetails.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <p v-if="!server">Merci de selectionner un serveur</p>
+        <p v-else>Server #{{server.id}} selected, Status: {{server.status}}</p>
+        <hr>
+        <button @click="resetStatus">Revenir à Normal</button>
+    </div>
+
+</template>
+
+<script>
+import {serverBus} from '../../main';
+
+export default {
+    data : function() {
+        return {
+            server: null
+        }
+    },
+    methods:{
+        resetStatus(){
+            this.server.status =  'Normal';
+        }
+    },
+    created(){
+        serverBus.$on('serverSelected', (server) => {
+            this.server = server;
+        });
+    }
+}
+</script>
+
+<style>
+
+</style>
